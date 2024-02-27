e2fb31ebf33d386e319a0936ffa8186e7c790804
hamed18 <hamedcnec@gmail.com>
2024-02-28 00:23:50 +0600

    final create section

--- create.php
+++ create.php
@@ -1,32 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>my shop</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-</head>
-
-<body>
-    <div class="container my-5">
-        <h2>list of client</h2>
-        <a href="" role="button" class="btn btn-primary">new client</a>
-        <br>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>NAME</th>
-                    <th>EMAIL</th>
-                    <th>PHONE</th>
-                    <th>ADDRESS</th>
-                    <th>CREATED AT</th>
-                    <th>ACTION</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
+// database connection
 // Database credentials
 $servername = "localhost";
 $username = "root";
@@ -36,39 +10,128 @@ $database = "myshop";
 // Create connection
 $connection = new mysqli($servername, $username, $password, $database);
 
-// Check connection
-if ($connection->connect_error) {
-    die("Connection failed: " . $connection->connect_error);
-}
-// read all row from database table
-$sql="SELECT *FROM clients";
-$result = $connection->query($sql);
 
-if (!$result){
-	die("Invalid query: " . $connection->connection->error);
-}
+$name = "";
+$email = "";
+$phone = "";
+$address = "";
 
-// read data of each row
-while($row = $result->fetch_assoc()){
-	echo "
-	<tr>
-	 <td>$row[id]</td>
-	 <td>$row[name]</td>
-	 <td>$row[email]</td>
-	 <td>$row[phone]</td>
-	 <td>$row[address]</td>
-	 <td>$row[created_at]</td>
-	 <td>
-		<a class='btn btn-primary btn-sm' href='/myshop/edit.php?id=$row[id]' >Edit</a>
-		<a class='btn btn-danger btn-sm' href='/myshop/delete.php?id=$row[id]' >Delete</a>
-	 </td>
-    </tr>
-	";
-}
+$errorMessage = "";
+$successMessage = "";
 
+if ( $_SERVER['REQUEST_METHOD'] == 'POST'){      // POST - data transmit
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$phone = $_POST["phone"];
+	$address = $_POST["address"];
+
+	do{
+       if (empty($name) || empty($email) || empty($phone) || empty($address)){
+		  $errorMessage = "All the fields are required";
+		  break;
+	   }
+
+	   // add new client to database
+       $sql = "INSERT INTO clients (name, email, phone, address)" .
+              "VALUES ('$name', '$email', '$phone', '$address')";
+       $result = $connection->query($sql);
+
+	   if (!$result){
+		 $errorMessage = "Invalid query: " . $connection->error;
+		 break;
+	   }
+
+	   $name = "";
+	   $email = "";
+	   $phone = "";
+	   $address = "";
+
+	   $successMessage = "Client added correctly";
+
+	   header("location: /myshop/index.php");
+	   exit;
+	   
+	} while(false);
+}
 ?>
-            </tbody>
-        </table>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>my shop</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"> </script>
+
+</head>
+
+<body>
+    <div class="container my-5">
+        <h2>New Client</h2>
+
+        <?php
+		 if (!empty($errorMessage)){
+			echo "
+			<div class='alert alert-warning alert-dismissable fade show' role='alert'>
+			   <strong>$errorMessage</strong>
+			   <button type = 'button' class = 'btn-close' data-bs-dismiss='alert' aria-label='close'></button>
+			</div>
+			";
+		 }
+		 ?>
+
+        <form method="post">
+            <div class="row mb-3">
+                <label for="" class="col-sm-3 col-form-label">Name</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="name" value="<?php echo $name; ?>">
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label for="" class="col-sm-3 col-form-label">Email</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="email" value="<?php echo $email; ?>">
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label for="" class="col-sm-3 col-form-label">Phone</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="phone" value="<?php echo $phone; ?>">
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label for="" class="col-sm-3 col-form-label">Address</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="address" value="<?php echo $address; ?>">
+                </div>
+            </div>
+
+            <?php 
+			if (!empty($successMessage)){
+				echo "
+				<div class='row mb-3'>
+					<div class='offset-sm-3 col-sm-6'>
+					   <div class='alert alert-success alert-dismissable fade show' role='alert'>
+					       <strong>$successMessage</strong>
+					       <button type = 'button' class = 'btn-close' data-bs-dismiss='alert' aria-label='close'></button>
+				       </div>
+					</div>
+				</div>
+				";
+			 }
+			?>
+
+            <div class="row mb-3">
+                <div class="offset-sm-3 d-grid">
+                    <button class="btn btn-primary" type="submit">submit</button>
+                </div>
+                <div class="col-sm-3 d-grid">
+                    <a class="btn btn-outline-primary" href="/myshop/index.php" role="button">cancel</a>
+                </div>
+            </div>
+        </form>
     </div>
 </body>
 
