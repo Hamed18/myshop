a1f466fc5f0bca2a5d722ac5ca13e003eec21677
hamed18 <hamedcnec@gmail.com>
2024-02-28 01:48:57 +0600

    delete section

---
+++ delete.php
@@ -0,0 +1,21 @@
+<?php 
+if ( isset($_GET["id"]) ) {   // check it receive the id or not
+	$id = $_GET["id"];
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$database = "myshop";
+
+	// create connection
+	$connection = new mysqli($servername, $username, $password, $database);
+
+	$sql = "DELETE FROM clients WHERE id="$id";
+	$connection->query($sql);
+
+}
+
+header("location: /myshop/index.php");   
+exit;
+
+?>
