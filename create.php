8343dce9d1a478776d1c83db1ac3a34dbdc2dff9
hamed18 <hamedcnec@gmail.com>
2024-02-27 22:26:33 +0600

    fixing syntax error

--- create.php
+++ create.php
@@ -15,8 +15,8 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST'){      // POST - data transmit
 
 	do{
        if (empty($name) || empty($email) || empty($phone) || empty($address)){
-		$errorMessage = "All the fields are required";
-		break;
+		  $errorMessage = "All the fields are required";
+		  break;
 	   }
 
 	   // add new client to database
@@ -25,7 +25,7 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST'){      // POST - data transmit
 	   $phone = "";
 	   $address = "";
 
-	   $successMessage = "Client added correctly"
+	   $successMessage = "Client added correctly";
 	   
 	} while(false);
 }
@@ -33,18 +33,21 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST'){      // POST - data transmit
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>my shop</title>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"> </script>
-</head>
-<body>
-	<div class="container my-5">
-         <h2>New Client</h2>
 
-         <?php
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>my shop</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"> </script>
+
+</head>
+
+<body>
+    <div class="container my-5">
+        <h2>New Client</h2>
+
+        <?php
 		 if (!empty($errorMessage)){
 			echo "
 			<div class='alert alert-warning alert-dismissable fade show' role='alert'>
@@ -55,36 +58,36 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST'){      // POST - data transmit
 		 }
 		 ?>
 
-		 <form method="post">
-             <div class="row mb-3">
+        <form method="post">
+            <div class="row mb-3">
                 <label for="" class="col-sm-3 col-form-label">Name</label>
-				<div class="col-sm-6">
-					<input type="text" class="form-control" name="name" value="<?php echo $name; ?>">
-				</div>
-			 </div>
-             <div class="row mb-3">
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="name" value="<?php echo $name; ?>">
+                </div>
+            </div>
+            <div class="row mb-3">
                 <label for="" class="col-sm-3 col-form-label">Email</label>
-				<div class="col-sm-6">
-					<input type="text" class="form-control" name="email" value="<?php echo $email; ?>">
-				</div>
-			 </div>
-             <div class="row mb-3">
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="email" value="<?php echo $email; ?>">
+                </div>
+            </div>
+            <div class="row mb-3">
                 <label for="" class="col-sm-3 col-form-label">Phone</label>
-				<div class="col-sm-6">
-					<input type="text" class="form-control" name="phone" value="<?php echo $phone; ?>">
-				</div>
-			 </div>
-             <div class="row mb-3">
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="phone" value="<?php echo $phone; ?>">
+                </div>
+            </div>
+            <div class="row mb-3">
                 <label for="" class="col-sm-3 col-form-label">Address</label>
-				<div class="col-sm-6">
-					<input type="text" class="form-control" name="address" value="<?php echo $address; ?>">
-				</div>
-			 </div>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="address" value="<?php echo $address; ?>">
+                </div>
+            </div>
 
-			<?php 
+            <?php 
 			if (!empty($successMessage)){
 				echo "
-				<div class="row mb-3">
+				<div class='row mb-3'>
 					<div class='offset-sm-3 col-sm-6'>
 					   <div class='alert alert-success alert-dismissable fade show' role='alert'>
 					       <strong>$successMessage</strong>
@@ -95,16 +98,17 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST'){      // POST - data transmit
 				";
 			 }
 			?>
-		   
+
             <div class="row mb-3">
-				<div class="offset-sm-3 d-grid">
-					<button class="btn btn-primary" type="submit">submit</button>
-				</div>
-				<div class="col-sm-3 d-grid">
-					<a class="btn btn-outline-primary" href="/myshop/index.php" role="button">cancel</a>
-				</div>
-			</div>
-		</form>
-	</div>
+                <div class="offset-sm-3 d-grid">
+                    <button class="btn btn-primary" type="submit">submit</button>
+                </div>
+                <div class="col-sm-3 d-grid">
+                    <a class="btn btn-outline-primary" href="/myshop/index.php" role="button">cancel</a>
+                </div>
+            </div>
+        </form>
+    </div>
 </body>
+
 </html>
